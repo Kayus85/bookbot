fdfa36d297b4185eb0d0e6907d627f2795c9de3c
kayus85 <kayode.nwanze@gmail.com>
2025-01-22 21:58:55 -0500

    added book report

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ def main(path_to_file):
     file_contents = get_book_text(path_to_file)
     number_of_words = count_words(file_contents)
     char_stats = count_char(file_contents)
-    print(char_stats)
-    # print(number_of_words)
+    filtered_characters = sort_and_filter(char_stats)
+
+    # print report
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{number_of_words} words found in document")
+    print("\n")
+    for e in filtered_characters:
+        print(f"The '{e["char"]}' character was found {e["count"]} times")
+    print("--- End report ---")
 
 def get_book_text(file):
     with open(file) as f:
@@ -22,5 +29,28 @@ def count_char(text):
         else:
             char_dict[char] = 1
     return char_dict
+
+def sort_and_filter(dict):
+    list_from_dict = []
+    for e in dict:
+        list_from_dict.append({'char': e, 'count': dict[e]})
+
+    list_from_dict.sort(key=sort_on_count, reverse=True)
+
+    filtered = filter_alpha(list_from_dict)
+    return filtered
+
+
+
+
+def sort_on_count(dict):
+    return dict["count"]
+
+def filter_alpha(list):
+    only_alpha = []
+    for element in list:
+        if element["char"].isalpha():
+            only_alpha.append(element)
+    return only_alpha
 
 main("books/frankenstein.txt")
